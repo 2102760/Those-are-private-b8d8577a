8aba9a6aea1ee45ffedfa6dc50b154d763f1b751
BeauvR <beauvrmc140@gmail.com>
2020-05-28 10:58:15 +0200

    uitwerking

--- MyLogger.class.php
+++ MyLogger.class.php
@@ -10,35 +10,41 @@ class MyLogger
                 break;
             case 'debug':
                 $this->debug($message);
-            case 'notice' || 'info':
+            case 'notice':
                 $this->notice($message);
                 break;
             case 'warning':
                 $this->warning($message);
                 break;
             default:
-                error_log('unknown log type');
+                echo('unknown log type');
         }
     }
 
     public function error($message)
     {
-        error_log('Error: ' . $message);
+        $this->logWithTime('Error: ' . $message);
     }
 
     public function debug($message)
     {
-        error_log('Debug: ' . $message);
+        $this->logWithTime('Debug: ' . $message);
     }
 
     public function notice($message)
     {
-        error_log('Notice: ' . $message);
+        $this->logWithTime('Notice: ' . $message);
     }
 
     public function warning($message)
     {
-        error_log('Warning: ' . $message);
+        $this->logWithTime('warning', $message);
+    }
+
+    private function logWithTime($type, $message)
+    {
+        $date = date('Y-m-d H:i:s');
+        echo('[' . $date . '] ' . $type . ': ' . $message);
     }
 }
 
